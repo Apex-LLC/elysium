d914b8ad2a8040ad4426401a23380d31ac6b47ed
Joe Bauer <Joe@ApexLLC.com>
2017-12-27 10:27:50 -0800

    trying to fix the migrations

--- db/migrate/20171024144600_add_user_reference_to_tenants.rb
+++ db/migrate/20171024144600_add_user_reference_to_tenants.rb
@@ -1,6 +1,6 @@
 class AddUserReferenceToTenants < ActiveRecord::Migration[5.1]
   def change
-    remove_reference :users, :tenant, foreign_key: true
+    # remove_reference :users, :tenant, foreign_key: true
     add_reference :tenants, :user, foreign_key: true
   end
 end
