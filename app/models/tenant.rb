cc2d2305fbfa7b22db64ebe7e73b0c141c501cb3
Joe Bauer <joebauer777@gmail.com>
2019-04-23 16:47:10 -0400

    Undid the last commit. Billable meter can't exist without associated tenant without some more code changes

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,6 +1,6 @@
 class Tenant < ApplicationRecord
   has_many :invoices, dependent: :destroy
-  has_many :billable_meters
+  has_many :billable_meters, dependent: :destroy
   has_many :payments
   has_many :tenant_users
   has_many :users, through: :tenant_users, dependent: :destroy
