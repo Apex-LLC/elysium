f8d07e6f371cf5de969465cdd1d57e558f9bcf5d
Joe Bauer <joebauer777@gmail.com>
2019-07-17 13:57:09 -0400

    Updated tenant welcome email with images and more polished layout.

--- app/mailers/tenant_mailer.rb
+++ app/mailers/tenant_mailer.rb
@@ -7,6 +7,13 @@ class TenantMailer < ApplicationMailer
   #   en.tenant_mailer.new_tenant_notification.subject
   #
   def new_tenant_notification(tenant, current_user)
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/logo.png")
+    attachments.inline['facebook@2x.png'] = File.read("#{Rails.root}/app/assets/images/facebook@2x.png")
+    attachments.inline['linkedin@2x.png'] = File.read("#{Rails.root}/app/assets/images/linkedin@2x.png")
+    attachments.inline['website@2x.png'] = File.read("#{Rails.root}/app/assets/images/website@2x.png")
+    attachments.inline['laptop.png'] = File.read("#{Rails.root}/app/assets/images/laptop.png")
+
+
     @tenant = tenant
     @user = current_user
 
